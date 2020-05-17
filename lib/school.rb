d3e8bbf8b16482aeafed12e0b7deefe82913c0f8
Rene Avila <reneavila1993@gmail.com>
2020-05-17 18:21:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,15 @@
 # code here!
 class School
-  
+
+  attr_accessor :name, :grade
+
   def initialize name, roster = {}
     @name = name
     @roster = roster
+  end
+  
+  def add_student name, grade
+    roster[@name] = @grade
   end
   
  
